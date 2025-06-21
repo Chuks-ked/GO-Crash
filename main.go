f929df9416e464728550ffa764c923d8e13ceaf2
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-06-21 18:11:24 +0100

    api

--- main.go
+++ main.go
@@ -1,35 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+var ShortGo = "Watch GO crash course."
+var TradingCourse = "Watch Trading crash course."
+var Leisure = "Rest and get back to work."
+var taskItems = []string {ShortGo, TradingCourse, Leisure}
+
 
 
 func main()  {
-	fmt.Println("##### Hello Chuks, welcome to our Todolist App #####")
-
-	var ShortGo = "Watch GO crash course."
-	var TradingCourse = "Watch Trading crash course."
-	var Leisure = "Rest and get back to work."
-	var taskItems = []string {ShortGo, TradingCourse, Leisure}
-
-	printTasks(taskItems)
-	fmt.Println()
-	taskItems = addTasks(taskItems, "Go for a run")
-
-	fmt.Println("updated list")
-	printTasks(taskItems)
+	http.HandleFunc("/", helloUser)
+	http.HandleFunc("/show-tasks", showTasks)
+	http.ListenAndServe(":8080", nil)
 }
-
-
-func printTasks(taskItems []string) {
-	fmt.Println("List of my Todos")
-	for index, task := range taskItems {
-		// fmt.Println(index+1,".", task)
-		fmt.Printf("%d: %s\n", index+1, task)
+func showTasks(writer http.ResponseWriter, request *http.Request){
+	for _, task := range taskItems {
+		fmt.Fprintln(writer, task)
 	}
 }
 
-
-func addTasks(taskItems []string, newTask string) []string {
-	var updatedTaskItems = append(taskItems, newTask)
-	return updatedTaskItems
+func helloUser(writer http.ResponseWriter, request *http.Request){
+	var greeting = "Welcome to our todolist APP"
+	fmt.Fprintln(writer, greeting)
 }
+
+
+
+
+// func printTasks(taskItems []string) {
+// 	fmt.Println("List of my Todos")
+// 	for index, task := range taskItems {
+// 		// fmt.Println(index+1,".", task)
+// 		fmt.Printf("%d: %s\n", index+1, task)
+// 	}
+// }
+
+
+// func addTasks(taskItems []string, newTask string) []string {
+// 	var updatedTaskItems = append(taskItems, newTask)
+// 	return updatedTaskItems
+// }
