5cf1717c7d78373a5420358ab793766fef90fb43
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-06-21 17:17:37 +0100

    functions

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import "fmt"
 
 
+var ShortGo = "Watch GO crash course."
+var TradingCourse = "Watch Trading crash course."
+var Leisure = "Rest and get back to work."
+var taskItems = []string {ShortGo, TradingCourse, Leisure}
+
+
 func main()  {
-	ShortGo := "Watch GO crash course."
-	TradingCourse := "Watch Trading crash course."
-	Leisure := "Rest and get back to work."
-
-	taskItems := []string {ShortGo, TradingCourse, Leisure}
-
 	fmt.Println("##### Hello Chuks, welcome to our Todolist App #####")
+	printTasks()
+}
 
+
+func printTasks() {
+	fmt.Println("List of my Todos")
 	for index, task := range taskItems {
 		// fmt.Println(index+1,".", task)
 		fmt.Printf("%d: %s\n", index+1, task)
