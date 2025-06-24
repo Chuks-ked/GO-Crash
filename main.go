fb3504f15b2ff8c7c60992abdcee78619d751ce2
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-06-24 11:35:01 +0100

    pointers and memory storage

--- main.go
+++ main.go
@@ -1,46 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "fmt"
 
-var ShortGo = "Watch GO crash course."
-var TradingCourse = "Watch Trading crash course."
-var Leisure = "Rest and get back to work."
-var taskItems = []string {ShortGo, TradingCourse, Leisure}
+func main() {
+    x := 110
+    p := &x // p holds the memory address of x
 
-
-
-func main()  {
-	http.HandleFunc("/", helloUser)
-	http.HandleFunc("/show-tasks", showTasks)
-	http.ListenAndServe(":8080", nil)
+    fmt.Println("Value of x:", x)
+    fmt.Println("Address of x:", p)
+    fmt.Println("Value at address p points to:", *p)
 }
-func showTasks(writer http.ResponseWriter, request *http.Request){
-	for _, task := range taskItems {
-		fmt.Fprintln(writer, task)
-	}
-}
-
-func helloUser(writer http.ResponseWriter, request *http.Request){
-	var greeting = "Welcome to our todolist APP"
-	fmt.Fprintln(writer, greeting)
-}
-
-
-
-
-// func printTasks(taskItems []string) {
-// 	fmt.Println("List of my Todos")
-// 	for index, task := range taskItems {
-// 		// fmt.Println(index+1,".", task)
-// 		fmt.Printf("%d: %s\n", index+1, task)
-// 	}
-// }
-
-
-// func addTasks(taskItems []string, newTask string) []string {
-// 	var updatedTaskItems = append(taskItems, newTask)
-// 	return updatedTaskItems
-// }
